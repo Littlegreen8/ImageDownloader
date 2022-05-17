ffe039c89404a4fd8aad52da46d68beba71e3c6e
Littlegreen8 <pzen0002@student.monash.edu>
2022-05-18 00:59:19 +1000

    Add project files.

---
+++ ImageDownloader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace ImageDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(1000);
+
+            var driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http:")
+        }
+    }
+}
